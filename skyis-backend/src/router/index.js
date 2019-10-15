55565794700e42a46b11b918976fb3e6fc10c030
heoin122@gmail.com <heoin122@gmail.com>
2019-10-15 17:53:31 +0900

    10.15 Routing

--- skyis-backend/src/router/index.js
+++ skyis-backend/src/router/index.js
@@ -4,6 +4,7 @@ import food from  './food';
 import ingredient from './ingredient'
 import recommend from './recommend';
 import user from './user';
+import nutrient from './nutrient';
 
 const router = express.Router();
 
@@ -15,6 +16,7 @@ router.use('/*', (req, res, next) => {
     res.setHeader("Cache-Control", "must-revalidate, private");
     next();
 })
+router.use('/nutrient', nutrient);
 router.use('/analysis', analysis);
 router.use('/food', food);
 router.use('/ingredient', ingredient);
