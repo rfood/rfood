e15a771b2fc9eeaf520793ecc4d050d559349769
heoin122@gmail.com <heoin122@gmail.com>
2019-10-21 15:02:24 +0900

    Sequelize Doing 19.10.21

--- skyis-backend/src/router/food/index.js
+++ skyis-backend/src/router/food/index.js
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import {
-    Food
+    Food, FoodIngredient
 } from "../../database/models";
 import {Op} from "sequelize";
 
@@ -59,6 +59,68 @@ router.get('/:name', (req, res) => {
         console.log(err);
     })
 })
+
+/**
+ * Made by HeoIn
+ * Food 기본 정보 등록
+ *
+ * POST /api/food
+ *      body
+ *      {
+ *          "name": "음식이름",
+ *          "serving": "인분수",
+ *          "description": "설명",
+ *          "ingredients": [
+ *              {"id": 321, "amount": 200},
+ *              {"id": 320, "amount": 20}
+ *          ]
+ *      }
+ *
+ * ERROR CODE
+ *      1: BAD NAME ( 한글로 제한 )
+ *      2: BAD SERVING ( 숫자가 아닌지, 1만보다 큰지 )
+ */
+router.post('/', (req, res) => {
+    // Err 1 : 음식 이름 한글인지 체크
+    let nameKorRegex = /[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/g;
+    if(nameKorRegex.test(req.body.name)) {
+        return res.status(400).json({
+            error: "BAD NAME",
+            code: 1
+        });
+    }
+    // Err 2 : 인분 수 숫자인지 체크
+
+    let numberRegex = /[^0-9]/g;
+    if(numberRegex.test(req.body.serving) || req.body.serving > 10000) {
+        return res.status(400).json({
+            error: "BAD SERVING",
+            code: 2
+        });
+    }
+
+    Food.create({ name: req.body.name, serving: req.body.serving, description: req.body.description})
+        .then(result => {
+            let id = result.id;
+            res.json(result);
+            for(let ing in req.body.ingredients) {
+                FoodIngredient.create({ food_id: result.id, ingredient_id: req.body.ingredients[ing].id, ingredient_amount: req.body.ingredients[ing].amount})
+                    .then(result => {
+                        res.json(result);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        return res.status(409);
+                    })
+            }
+            res.status(200);
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(409);
+        });
+    return res.status(200);
+});
 
 
 
--- skyis-backend/src/router/nutrient/index.js
+++ skyis-backend/src/router/nutrient/index.js
@@ -3,13 +3,14 @@ const router = express.Router();
 import {
     Nutrient,
     Ingredient,
-    IngredientNutrient
+    IngredientNutrient,
+    Food, FoodIngredient
 } from "../../database/models";
 
 /**
  * Made by Heo In
  * 2019.10.21
- * Nutrient
+ * Nutrient (Ingredient)
  * Ingredient의 Nutrient를 받아온다
  *
  * GET /api/nutrient/ingredient/:id
@@ -38,7 +39,7 @@ router.get('/ingredient/:id', (req, res) => {
             }
         }],
         where: {id: req.params.id}
-    }).then(result => {
+    }).then(result => {id
         res.json(result);
         return res.status(200);
     }).catch(err => {
@@ -49,7 +50,7 @@ router.get('/ingredient/:id', (req, res) => {
 /**
  * Made by Heo In
  * 2019.10.21
- * Nutrient
+ * Nutrient (Ingredient)
  * Ingredient의 Nutrient를 받아온다
  *
  * GET /api/nutrient/ingredient/:id/:type
@@ -63,7 +64,7 @@ router.get('/ingredient/:id', (req, res) => {
  *      5: 식염
  *      6: 폐기율
  *      7: 무기질
- *      8: 비타민
+ *      8: 비타민Ingredient의
  *
  * Error Code
  *      1: BAD ID
@@ -107,5 +108,67 @@ router.get('/ingredient/:id/:type', (req, res) => {
     })
 });
 
+/**
+ * Made by Heo In
+ * 2019.10.21
+ * Nutrient (Fod)
+ * Food의 Nutrient를 받아온다
+ *
+ * GET /api/nutrient/food/:id
+ * Param : { id: 50 }
+ *
+ * Error Code
+ *      1: BAD ID
+ *
+ */
+router.get('/food/:id', (req, res) => {
+    let numberRegex = /[^0-9]/g;
+    if(numberRegex.test(req.params.id)) {
+        return res.status(400).json({
+            error: "BAD ID",
+            code: 1
+        });
+    }
+    Food.findOne({
+        attributes: ['name'],
+        include: [{
+            model: Ingredient,
+            include: [{
+                model: Nutrient,
+                attributes: ['id', 'name_kor', 'unit'],
+                through: {
+                    model: IngredientNutrient,
+                    attributes: ['nutrient_amount']
+                }
+            }]
+        }],
+        where: {id: req.params.id}
+    }).then(result => {
+        let allNut = {};
+        allNut["name"] = result.name;
+        allNut["nutrients"] = [];
+        for(let ingredient in result.ingredients) {
+            let am = result.ingredients[ingredient].food_ingredient.ingredient_amount / 100;
+            for(let nutrient in result.ingredients[ingredient].nutrients) {
+                if(allNut.nutrients[result.ingredients[ingredient].nutrients[nutrient].id] == null) {
+                    allNut.nutrients[result.ingredients[ingredient].nutrients[nutrient].id] = {
+                        "amount": 0,
+                        "name_kor": "",
+                        "unit": ""
+                    }
+                }
+                console.log(result.ingredients[ingredient].nutrients[nutrient].ingredient_nutrient.nutrient_amount);
+                allNut.nutrients[result.ingredients[ingredient].nutrients[nutrient].id].amount += result.ingredients[ingredient].nutrients[nutrient].ingredient_nutrient.nutrient_amount * am;
+                allNut.nutrients[result.ingredients[ingredient].nutrients[nutrient].id].name_kor = result.ingredients[ingredient].nutrients[nutrient].name_kor;
+                allNut.nutrients[result.ingredients[ingredient].nutrients[nutrient].id].unit = result.ingredients[ingredient].nutrients[nutrient].unit;
+            }
+        }
+        console.log(allNut);
+        res.json(result);
+        return res.status(200);
+    }).catch(err => {
+        console.log(err);
+    })
+});
 
 export default router;
